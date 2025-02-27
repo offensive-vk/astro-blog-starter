444176ade044b7de84e0f09412cb461776e4f8b2
Vedansh <superuser.ntsystems@outlook.com>
2025-02-27 18:40:07 +0530

    feat: improvements + optimized.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { SITE_TITLE, SITE_DESCRIPTION, SITE_SOURCE, SITE_OWNER, SITE_TEMPLATE } from "../consts";
+import { SITE_TITLE, SITE_DESCRIPTION, SITE_SOURCE } from "../consts";
 
 export async function GET(context) {
   const posts = await getCollection("blog");
